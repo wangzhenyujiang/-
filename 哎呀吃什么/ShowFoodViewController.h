106b31fcebedbe750e948c4a1654aa6e1282f3cc
YuYu <wangzhenyujiang@163.com>
2014-12-01 12:09:13 +0800

    添加了一些ViewController的界面类。

---
+++ 哎呀吃什么/ShowFoodViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowFoodViewController.h
+//  哎呀吃什么
+//
+//  Created by 王振宇 on 14/12/1.
+//  Copyright (c) 2014年 王振宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowFoodViewController : UIViewController
+
+@end
---
+++ 哎呀吃什么/ShowFoodViewController.m
@@ -0,0 +1,37 @@
+//
+//  ShowFoodViewController.m
+//  哎呀吃什么
+//
+//  Created by 王振宇 on 14/12/1.
+//  Copyright (c) 2014年 王振宇. All rights reserved.
+//
+
+#import "ShowFoodViewController.h"
+
+@interface ShowFoodViewController ()
+
+@end
+
+@implementation ShowFoodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
