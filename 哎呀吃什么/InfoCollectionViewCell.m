6f827d71a96d0dd68f64a87b30d11778fb785507
YuYu <wangzhenyujiang@163.com>
2014-12-01 12:01:40 +0800

    添加了两个cell类

---
+++ 哎呀吃什么/InfoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  InfoCollectionViewCell.m
+//  哎呀吃什么
+//
+//  Created by 王振宇 on 14/12/1.
+//  Copyright (c) 2014年 王振宇. All rights reserved.
+//
+
+#import "InfoCollectionViewCell.h"
+
+@implementation InfoCollectionViewCell
+
+@end
